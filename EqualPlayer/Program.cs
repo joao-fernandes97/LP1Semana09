e342c199b9180035cad35d053c3d0aa8b35dc31d
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-05-10 17:01:28 +0100

    setOfPlayers variable

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -6,7 +6,10 @@ namespace EqualPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HashSet<Player> setOfPlayers = new HashSet<Player>;
+            setOfPlayers.Add(new Player(PlayerClass.Tank, "Ana"));
+            setOfPlayers.Add(new Player(PlayerClass.Slayer, "Paulo"));
+            setOfPlayers.Add(new Player(PlayerClass.Tank, "Ana"));
         }
     }
 }
