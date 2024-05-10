a58e37a1ecc1b1b6d3bd9b0a63a86964e3de9db1
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-05-10 16:36:09 +0100

    create and populate enumerables

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntCollections
 {
@@ -6,7 +7,33 @@ namespace IntCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> intList = new List<int>() {1, 10, -30, 10, -5};
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(10);
+            intStack.Push(-30);
+            intStack.Push(10);
+            intStack.Push(-5);
+            Queue<int> intQ = new Queue<int>();
+            intQ.Enqueue(1);
+            intQ.Enqueue(10);
+            intQ.Enqueue(-30);
+            intQ.Enqueue(10);
+            intQ.Enqueue(-5);
+            HashSet<int> intHSet = new HashSet<int>() {1, 10, -30, 10, -5};
+
+            List<IEnumerable<int>> iterables = new List<IEnumerable<int>> {intList, intStack, intQ, intHSet}; 
+
+            foreach (var iterable in iterables)
+            {
+                Console.Write(iterable.GetType().Name.Split('`')[0] + ": ");
+                foreach (var item in iterable)
+                {
+                    //TODO: get rid of last comma
+                    Console.Write(item + ", ");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
