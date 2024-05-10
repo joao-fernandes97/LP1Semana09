4aa415b857dc6d294a63768ffd05031696327c13
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-05-10 17:34:30 +0100

    Parse files and print union of found integers

---
+++ IntGrouper/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IntGrouper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+            foreach (string filename in args)
+            {
+                string s;
+                using StreamReader sr = new StreamReader(filename);
+
+                while((s=sr.ReadLine()) != null)
+                {
+                    int i;
+                    bool isInt = int.TryParse(s, out i);
+                    
+                    if(!isInt)
+                    {
+                        Console.WriteLine("Found non Integer");
+                        return;
+                    }
+                    
+                    if(!(ints.Contains(i)))
+                    {
+                        ints.Add(i);
+                    }
+                }
+            }
+            ints.Sort();
+            foreach (var item in ints)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
